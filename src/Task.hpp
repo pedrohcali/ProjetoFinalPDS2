2c0bfdcfbe824e5fee631f1d93523467cf182042
carol-oliveiravs <135362827+carol-oliveiravs@users.noreply.github.com>
2023-07-04 19:53:09 -0300

    Update Task.hpp

--- src/Task.hpp
+++ src/Task.hpp
@@ -9,15 +9,25 @@ class Task {
     std::string dueDate;
 
 public:
+    // Lança std::invalid_argument se o titulo estiver vazio
     Task(const std::string& title, const std::string& description, const std::string& dueDate);
 
+    // Retorna o titulo da tarefa.
     std::string getTitle() const;
+
+    // Define o titulo da tarefa. Lança std::invalid_argument se o titulo estiver vazio.
     void setTitle(const std::string& title);
 
+    // Retorna a descricao da tarefa.
     std::string getDescription() const;
+
+    // Define a descricao da tarefa.
     void setDescription(const std::string& description);
 
+    // Retorna a data limite da tarefa.
     std::string getDueDate() const;
+
+    // Define a data limite da tarefa. 
     void setDueDate(const std::string& dueDate);
 };
 

843215dd203aa20715c5dec0c9c2c0e938f1c54e
carol-oliveiravs <135362827+carol-oliveiravs@users.noreply.github.com>
2023-07-04 19:42:03 -0300

    tratamento de erros na User.cpp

--- src/User.cpp
+++ src/User.cpp
@@ -2,6 +2,7 @@
 #include "Board.hpp"
 #include "Task.hpp"
 #include <algorithm>
+#include <stdexcept>
 
 User::User(const std::string& username, const std::string& email, TaskManager& taskManager)
     : username(username), email(email), taskManager(taskManager) {
@@ -15,6 +16,9 @@ const std::string& User::getUsername() const {
 }
 
 void User::setUsername(const std::string& username) {
+    if (username.empty()) {
+        throw std::invalid_argument("O nome de usuário não pode estar vazio.");
+    }
     this->username = username;
 }
 
@@ -23,6 +27,9 @@ const std::string& User::getEmail() const {
 }
 
 void User::setEmail(const std::string& email) {
+    if (email.empty() || email.find('@') == std::string::npos) {
+        throw std::invalid_argument("O endereço de e-mail fornecido é inválido.");
+    }
     this->email = email;
 }
 
@@ -31,6 +38,16 @@ const std::vector<std::unique_ptr<Board>>& User::getBoards() const {
 }
 
 void User::addBoard(std::unique_ptr<Board> board) {
+    if (!board) {
+        throw std::invalid_argument("Quadro não pode ser nulo.");
+    }
+
+    for (const auto& b : boards) {
+        if (b->getName() == board->getName()) {
+            throw std::invalid_argument("Já existe um quadro com o mesmo nome.");
+        }
+    }
+
     boards.push_back(std::move(board));
 }
 
@@ -40,14 +57,20 @@ void User::removeBoard(const std::string& boardName) {
         return board->getName() == boardName;
     });
     
-    if (it != boards.end()) {
-        boards.erase(it);
+    if (it == boards.end()) {
+        throw std::invalid_argument("Quadro não encontrado.");
     }
+    
+    boards.erase(it);
 }
 
 void User::moveTask(Board* fromBoard, Board* toBoard, Task* task) {
-    if(taskManager.moveTask(task, fromBoard, toBoard)) {
-        // Nada. Deu certo a movida. Talvez implementar validação de erros.
+    if (!fromBoard || !toBoard || !task) {
+        throw std::invalid_argument("Quadros e tarefa não podem ser nulos.");
+    }
+
+    if (!taskManager.moveTask(task, fromBoard, toBoard)) {
+        throw std::runtime_error("Falha ao mover a tarefa.");
     }
 }
 

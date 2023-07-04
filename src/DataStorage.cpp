41816b1f649dce65114ba57e599da9d50d6e0232
Isabela Valencia <133610762+belavc@users.noreply.github.com>
2023-07-03 21:47:51 -0300

    implementação metodo "save" e "load"

--- src/DataStorage.cpp
+++ src/DataStorage.cpp
@@ -12,16 +12,52 @@ DataStorage::~DataStorage() {
 void DataStorage::saveData() {
     std::ofstream file(filename);
     for (const auto& user : users) {
-        file << user->getUsername() << "\n";
+        file << user->getUsername() << ',' << user->getEmail();
+        for (const auto& board : user->getBoards()) {
+            file << ',' << board->getName() << ":{";
+            auto& tasks = board->getTasks();
+            for (size_t i = 0; i < tasks.size(); ++i) {
+                if (i != 0) {
+                    file << ';';
+                }
+                file << tasks[i]->getTitle() << ':' << tasks[i]->getDescription() << ':' << tasks[i]->getDueDate();
+            }
+            file << "}";
+        }
+        file << '\n';
     }
 }
 
 void DataStorage::loadData() {
     std::ifstream file(filename);
-    std::string username;
-    std::string email;
-    while (std::getline(file, username) && std::getline(file, email)) {
-        addUser(std::make_unique<User>(username, email, taskManager));
+    std::string line;
+    while (std::getline(file, line)) {
+        std::istringstream iss(line);
+        std::string username, email, token;
+        std::getline(iss, username, ',');
+        std::getline(iss, email, ',');
+        auto user = std::make_unique<User>(username, email, taskManager);
+        while (std::getline(iss, token, ',')) {
+            std::istringstream issBoard(token);
+            std::string boardName, tasksStr;
+            std::getline(issBoard, boardName, ':');
+            std::getline(issBoard, tasksStr, '{');
+            std::getline(issBoard, tasksStr, '}');
+            auto board = std::make_unique<Board>(boardName);
+            std::istringstream issTasks(tasksStr);
+            std::string taskStr;
+            while (std::getline(issTasks, taskStr, ';')) {
+                std::istringstream issTask(taskStr);
+                std::string taskTitle, taskDescription, taskDueDate;
+                std::getline(issTask, taskTitle, ':');
+                std::getline(issTask, taskDescription, ':');
+                std::getline(issTask, taskDueDate, ':');
+                Task* task = taskManager.createTask(taskTitle, taskDescription, taskDueDate);
+                board->addTask(task);
+            }
+            user->addBoard(std::move(board));
+        }
+        users.push_back(std::move(user));
     }
 }
 

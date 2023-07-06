0b58099f1037ddf805ecb234ff03f230b5c5153b
eduardachecon <138607186+eduardachecon@users.noreply.github.com>
2023-07-06 17:28:30 -0300

    Update Task.cpp

--- src/Task.cpp
+++ src/Task.cpp
@@ -1,6 +1,6 @@
 #include "Task.hpp"
-#include <regex>
-#include <stdexcept>
+#include <regex>  // Será utilizada para validar o formato da data de vencimento
+#include <stdexcept>  // Será utilizada para lançar exceções caso ocorra algum erro
 
 Task::Task(const std::string& title, const std::string& description, const std::string& dueDate)
 : title(title), description(description), dueDate(dueDate) {}
@@ -9,6 +9,7 @@ std::string Task::getTitle() const {
     return title;
 }
 
+// Define o título da tarefa
 void Task::setTitle(const std::string& title) {
     if (title.empty()) {
         throw std::invalid_argument("Titulo nao pode ser vazio");
@@ -28,6 +29,7 @@ std::string Task::getDueDate() const {
     return dueDate;
 }
 
+// Define a data de vencimento da tarefa
 void Task::setDueDate(const std::string& dueDate) {
     std::regex datePattern("\\d{4}-\\d{2}-\\d{2}");
     if (!std::regex_match(dueDate, datePattern)) {
